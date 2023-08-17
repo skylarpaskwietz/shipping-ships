76836eb665254b949e5f627d2c558741681940a6
Skylar Paskwietz <skypaskwietz22@gmail.com>
2023-08-17 15:19:19 -0500

    added html

---
+++ scripts/DockList.js
@@ -0,0 +1,17 @@
+import { getDocks } from "./database.js";
+
+export const DockList = () => {
+    const docks = getDocks()
+
+    let docksHTML = "<ul>"
+
+    for (const dock of docks) {
+        docksHTML += `<li data-id="${dock.id}"
+                          data-location="${dock.location}"
+                          data-volume="${dock.volume}"
+                          </li>`
+        
+    }   docksHTML += "</ul>"
+
+    return docksHTML
+}
